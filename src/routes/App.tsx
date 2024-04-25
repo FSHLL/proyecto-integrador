3cc8ab8fa7e4a97bbbe8076d3bed7f33d435c6d1
Freder Steven Hernandez Llaten <freder.hernandez@evertecinc.com>
2024-04-25 11:10:07 -0500

    feat: improve in logo and menu for characters path

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,14 +1,43 @@
-import { Layout } from 'antd';
+import { Layout, Menu, MenuProps } from 'antd';
 import { Content, Footer, Header } from 'antd/es/layout/layout';
-import { Outlet } from "react-router-dom";
+import { useState } from 'react';
+import { Link, Outlet, useNavigate } from "react-router-dom";
 
 export const App = () => {
+
+  const [current, setCurrent] = useState('');
+
+  const navigate = useNavigate()
+
+  const menuItems: MenuProps['items'] = [
+    {
+      key: '1',
+      label: <Link to={'characters'}>Character</Link>
+    }
+  ]
+
+  const onClick: MenuProps['onClick'] = (e) => {
+    setCurrent(e.key);
+  };
+
+  const goHome = () => {
+    navigate('/')
+    setCurrent('');
+  }
 
   return (
     <>
       <Layout style={{ minHeight: '100vh' }}>
-        <Header>
-          Header
+        <Header style={{ display: 'flex', alignItems: 'center' }}>
+          <img onClick={goHome} height={70} src="https://universate.co/wp-content/uploads/2022/05/logo-universidad-del-valle.png" alt="log"/>
+          <Menu
+            onClick={onClick}
+            theme="dark"
+            mode="horizontal"
+            items={menuItems}
+            selectedKeys={[current]}
+            style={{ flex: 1, minWidth: 0, margin: 20 }}
+          />
         </Header>
         <Content style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
           <Outlet />
