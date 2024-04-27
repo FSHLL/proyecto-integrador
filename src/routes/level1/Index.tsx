305df0edb3176cee4e33be7962148a7d49380257
Freder Steven Hernandez Llaten <freder.hernandez@evertecinc.com>
2024-04-27 03:22:17 -0500

    feat: map1 and warrior models

--- src/routes/level1/Index.tsx
+++ src/routes/level1/Index.tsx
@@ -5,26 +5,31 @@ import { KeyboardControls } from "@react-three/drei";
 import { animationSet, keyboardMap } from '../../constants/joystick';
 import { Priest } from '@/models/Priest';
 import { getModelPath } from '@/helpers/path';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { playAudio, stopAudio } from "@/helpers/audio";
+import { Map1 } from "@/models/Map1";
+import { Warrior } from "@/models/Warrior";
+import { getRandomArbitrary } from "@/helpers/random";
+import { Trunk } from "@/components/Trunk";
+import { Vector3 } from "three";
 
 export const Index = () => {
 
-    const characterURL = getModelPath('priest')
+    const characterURL = getModelPath('warrior')
 
-    // const [trunksToShow, setTrunksToShow] = useState<JSX.Element[]>([]);
+    const [trunksToShow, setTrunksToShow] = useState<JSX.Element[]>([]);
 
-    // useEffect(() => {
-    //     const timer = setTimeout(() => {
-    //         setTrunksToShow((prevTrunks) => {
-    //             const randomX = getRandomArbitrary(-10, 10);
-    //             const newTrunk = <Trunk key={prevTrunks.length} position={new Vector3(randomX, 0, 10)} />;
-    //             return [...prevTrunks, newTrunk];
-    //         });
-    //     }, 2000);
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setTrunksToShow((prevTrunks) => {
+                const randomX = getRandomArbitrary(-100, 100);
+                const newTrunk = <Trunk key={prevTrunks.length} position={new Vector3(randomX, 0, 20)} />;
+                return [...prevTrunks, newTrunk];
+            });
+        }, 1000);
 
-    //     return () => clearTimeout(timer);
-    // });
+        return () => clearTimeout(timer);
+    });
 
     // @ts-expect-error State types unavailable
     const curAnimation: string = useGame((state) => state.curAnimation)
@@ -56,20 +61,24 @@ export const Index = () => {
                         characterURL={characterURL}
                         animationSet={animationSet}
                     >
-                        <Priest position-y={-0.7} />
+                        <Warrior position-y={-0.7}/>
+                        {/* <Priest position-y={-0.7} /> */}
                     </EcctrlAnimation>
                 </Ecctrl>
             </KeyboardControls>
 
-            {/* STAGE */}
-            <RigidBody type="fixed" position={[0, 0, 48]}>
+            <RigidBody type="fixed" colliders="trimesh" ccd>
+                <Map1 position={[0, -11, 0]}/>
+            </RigidBody>
+
+            {/* <RigidBody type="fixed" position={[0, -10, 0]}>
                 <mesh receiveShadow>
                     <boxGeometry args={[100, 0.5, 100]} />
                     <meshStandardMaterial color="white" />
                 </mesh>
-            </RigidBody>
+            </RigidBody> */}
 
-            {/* {trunksToShow} */}
+            {trunksToShow}
         </>
     );
 };
