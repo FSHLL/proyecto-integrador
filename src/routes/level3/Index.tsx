85729128e98afc58c7984f602a376ba1e96b9303
Freder Hernandez <fredersteven399@hotmail.com>
2024-06-01 14:40:47 -0500

    feat: improve in controller actions

---
+++ src/routes/level3/Index.tsx
@@ -0,0 +1,288 @@
+import { CollisionEnterPayload, CylinderCollider, RapierRigidBody, RigidBody } from "@react-three/rapier";
+// @ts-expect-error No Types for Ecctrl
+import Ecctrl, { EcctrlAnimation, useGame } from "ecctrl";
+import { Cylinder, Html, KeyboardControls } from "@react-three/drei";
+import { animationSet, keyboardMap } from '../../constants/joystick';
+// import { Priest } from '@/models/Priest';
+import { getModelPath } from '@/helpers/path';
+import { useEffect, useRef, useState } from "react";
+import { playAudio, stopAudio } from "@/helpers/audio";
+import { Map1 } from "@/models/Map1";
+import { Warrior } from "@/models/Warrior";
+// import { getRandomArbitrary } from "@/helpers/random";
+// import { Trunk } from "@/components/Trunk";
+import { Vector3 } from "three";
+import { useCheckpoint } from "@/stores/useCheckpoint";
+import { LoadingScreen } from "@/components/LoadingScreen";
+import { player } from "@/constants/colliders";
+import { Bullet } from "@/components/Bullet";
+import { Bullet as TypeBullet } from "@/Interfaces/Bullet";
+import { Demon } from "@/models/Demon";
+// import * as THREE from 'three';
+import { CharacterController } from "@/components/CharacterController";
+// import { direction2Points } from "@/helpers/distance";
+// import { Vector } from "@dimforge/rapier3d-compat";
+import { Pigman } from "@/models/Pigman";
+import { gameStates, useGame as useLocalGame } from "@/stores/useGame";
+import { GameOver } from "@/components/GameOver";
+import { useCharacter } from "@/stores/useCharacter";
+import { rewards } from "@/constants/rewards";
+import { Reward } from "@/Interfaces/Reward";
+import { Cross } from "@/models/Cross";
+import { Win } from "@/components/Win";
+import Checkpoint from "@/components/Checkpoint";
+// import { rewards } from "./rewards";
+// import { Reward } from "@/Interfaces/Reward";
+// import { Cross } from "@/models/Cross";
+
+export const Index = () => {
+
+    const characterURL = getModelPath('warrior');
+
+    const characterRef = useRef<RapierRigidBody>();
+
+    const curCheckpoint = useCheckpoint((state) => state.curCheckpoint);
+
+    const setCurCheckpoint = useCheckpoint((state) => state.setCurCheckpoint);
+
+    const setCharacterRef = useCharacter((state) => state.setCharacterRef)
+
+    // const [trunksToShow, setTrunksToShow] = useState<JSX.Element[]>([]);
+
+    const [velocity, setVelocity] = useState<number>(2.5);
+
+    const [ecctrlMode, setEcctrlMode] = useState<string | null>(null);
+
+    const [loading, setLoading] = useState(false);
+
+    const [bullets, setBullets] = useState<TypeBullet[]>([]);
+
+    // @ts-expect-error State types unavailable
+    const curAnimation = useGame((state) => state.curAnimation);
+
+    // @ts-expect-error State types unavailable
+    const setMoveToPoint = useGame((state) => state.setMoveToPoint);
+
+    const gameState = useLocalGame((state) => state.gameState);
+
+    const setCurLevel = useLocalGame((state) => state.setCurLevel);
+
+    const [availableRewards, setAvailableRewards] = useState<Reward[]>([]);
+
+    const storeRewards = useLocalGame((state) => state.rewards);
+
+    const [showPigman, setShowPigman] = useState(false);
+
+    const [boosDeath, setBoosDeath] = useState(false);
+
+    const inCheckpoint = (coll: CollisionEnterPayload, id: number) => {
+        console.log(coll);
+        setEcctrlMode(null)
+        setVelocity(2.5)
+        setLoading(false);
+        if (id === 4) {
+            setShowPigman(true)
+        }
+    };
+
+    // const launchBullet = (position?: Vector) => {
+    //     const demonPosition = position;
+    //     const characterPosition = characterRef.current?.translation()
+
+    //     if (demonPosition && characterPosition) {
+    //         const bulletPosition = demonPosition;
+
+    //         const direction = direction2Points(characterPosition, demonPosition)
+
+    //         const bulletAngle = Math.atan2(direction.x, direction.z);
+
+    //         const bullet = {
+    //             id: (new Date()).toTimeString(),
+    //             position: vec3(bulletPosition),
+    //             angle: bulletAngle,
+    //         };
+
+    //         setBullets((bullets) => [...bullets, bullet]);
+    //     }
+    // };
+
+    const onHit = (bulletId: string) => {
+        setBullets((bullets) => bullets.filter((bullet) => bullet.id !== bulletId));
+    };
+
+    useEffect(() => {
+        if ([animationSet.walk, animationSet.run].includes(curAnimation) && !loading) {
+            playAudio(curAnimation.toLowerCase());
+        } else {
+            stopAudio();
+        }
+    }, [curAnimation, loading]);
+
+    useEffect(() => {
+        if (curCheckpoint && curCheckpoint.position) {
+            setLoading(true);
+            setEcctrlMode('PointToMove');
+            setVelocity(14);
+            setMoveToPoint(new Vector3(curCheckpoint.position.x, -0.7, curCheckpoint.position.z));
+        }
+        setCurLevel(1)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    useEffect(() => {
+        setCharacterRef(characterRef)
+    }, [characterRef, setCharacterRef])
+
+    useEffect(() => {
+        if (storeRewards) {
+            setAvailableRewards(rewards.filter(reward => !storeRewards.some(stored => stored.id === reward.id)))
+        }
+    }, [storeRewards])
+
+    const death = () => {
+        setBoosDeath(true)
+        setCurCheckpoint(null)
+    }
+
+    return (
+        <>
+            {/* <color attach="background" args={["#ececec"]} /> */}
+            <color attach="background" args={['#333333']} />
+
+            {/* LIGHTS */}
+            <ambientLight intensity={0.6} />
+            <directionalLight
+                position={[5, 5, 5]}
+                intensity={0.5}
+                castShadow />
+
+            <KeyboardControls map={keyboardMap}>
+                <Ecctrl ref={characterRef} name={player} mode={ecctrlMode} maxVelLimit={velocity} camInitDis={-6} animated>
+                    <EcctrlAnimation
+                        characterURL={characterURL}
+                        animationSet={animationSet}
+                    >
+                        <Warrior
+                            position-y={-0.9}
+                        />
+                        {/* <Priest position-y={-0.7} /> */}
+                    </EcctrlAnimation>
+                </Ecctrl>
+            </KeyboardControls>
+
+            {!loading &&
+                <RigidBody type="fixed" colliders={"trimesh"} ccd>
+                    <Map1 position={[-42, -10, 37]}/>
+                </RigidBody>
+            }
+
+            {loading &&
+                <RigidBody colliders={false} type="fixed" position={[-42, -6, 37]}>
+                    <CylinderCollider args={[0.5, 1000]} />
+                    <Cylinder scale={[1000, 1, 1000]} receiveShadow>
+                        {/* <meshStandardMaterial color={"transparent"} /> */}
+                    </Cylinder>
+                </RigidBody>
+            }
+
+            <Checkpoint id={1} level={1} position={new Vector3(-5, -5.2, 85)} onCollision={inCheckpoint} />
+            <Checkpoint id={2} level={1} position={new Vector3(-47, -5.2, 81)} onCollision={inCheckpoint} />
+            <Checkpoint id={3} level={1} position={new Vector3(-78,-5.5, 30)} onCollision={inCheckpoint} />
+            <Checkpoint id={4} level={1} position={new Vector3(-48.5 ,-5.5, 20)} onCollision={inCheckpoint} />
+
+            {!loading &&
+                <>
+                    <CharacterController position={[0,0,50]} moveSpeed={0.2}>
+                        <Demon />
+                    </CharacterController>
+
+                    <CharacterController position={[4,0,55]} moveSpeed={0.2}>
+                        <Demon />
+                    </CharacterController>
+
+                    <CharacterController position={[-4,0,55]} moveSpeed={0.2}>
+                        <Demon />
+                    </CharacterController>
+
+                    <CharacterController position={[-60, -4.4, 90]} moveSpeed={0.1}>
+                        <Demon />
+                    </CharacterController>
+
+                    <CharacterController position={[-65, -4.4, 92]} moveSpeed={0.1}>
+                        <Demon />
+                    </CharacterController>
+
+                    <RigidBody type="fixed" position={[17.8, -5, 53]}colliders={"trimesh"}>
+                        <mesh>
+                            <boxGeometry attach="geometry" args={[0.5, 5, 110]} />
+                            <meshStandardMaterial opacity={0.0} transparent />
+                        </mesh>
+                    </RigidBody>
+
+                    <RigidBody type="fixed" position={[-80.7, -5, 53]}colliders={"trimesh"}>
+                        <mesh>
+                            <boxGeometry attach="geometry" args={[0.5, 5, 110]} />
+                            <meshStandardMaterial  opacity={0.0} transparent />
+                        </mesh>
+                    </RigidBody>
+
+                    <RigidBody type="fixed" position={[-27, -5, -1.7]}colliders={"trimesh"}>
+                        <mesh>
+                            <boxGeometry attach="geometry" args={[110, 5, 0.5]} />
+                            <meshStandardMaterial opacity={0.0} transparent />
+                        </mesh>
+                    </RigidBody>
+
+                    <RigidBody type="fixed" position={[-27, -5, 97.8]}colliders={"trimesh"}>
+                        <mesh>
+                            <boxGeometry attach="geometry" args={[110, 5, 0.5]} />
+                            <meshStandardMaterial opacity={0.0} transparent />
+                        </mesh>
+                    </RigidBody>
+                   
+                    {showPigman &&
+                        <CharacterController death={death} position={[-80 ,0, 10]} damage={15} moveSpeed={0.1}>
+                            <Pigman scale={5}/>
+                        </CharacterController>
+                    }
+
+                    {
+                        (bullets).map((bullet: TypeBullet, index: number) => (
+                            <Bullet
+                                key={index}
+                                id={bullet.id}
+                                angle={bullet.angle}
+                                position={bullet.position}
+                                onHit={onHit} />
+                        ))
+                    }
+
+                    {
+                        (availableRewards).map((reward: Reward) => (
+                            <Cross
+                                scale={0.5}
+                                key={reward.id}
+                                reward={reward}
+                                position={reward.position}
+                            />
+                        ))
+                    }
+                </>
+            }
+
+            <Html>
+                <LoadingScreen loading={loading} setLoading={setLoading} />
+                {
+                    gameState === gameStates.GAME_OVER &&
+                    <GameOver />
+                }
+                {
+                    boosDeath &&
+                    <Win level="level2" />
+                }
+            </Html>
+
+            {/* {trunksToShow} */}
+        </>
+    );
+};
