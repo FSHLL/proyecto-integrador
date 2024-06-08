d5a49e4d2a245ea28bd54e67ab4014e1dbf8548d
Freder Hernandez <fredersteven399@hotmail.com>
2024-06-08 13:42:52 -0500

    feat: progress in boss 3

--- src/routes/level3/Index.tsx
+++ src/routes/level3/Index.tsx
@@ -1,16 +1,27 @@
-import { CollisionEnterPayload, CylinderCollider, RapierRigidBody, RigidBody } from "@react-three/rapier";
+import {
+  CollisionEnterPayload,
+  CylinderCollider,
+  RapierRigidBody,
+  RigidBody,
+  vec3,
+} from "@react-three/rapier";
 // @ts-expect-error No Types for Ecctrl
 import Ecctrl, { EcctrlAnimation, useGame } from "ecctrl";
-import { Cylinder, Html, KeyboardControls, OrbitControls } from "@react-three/drei";
-import { animationSet, keyboardMap } from '../../constants/joystick';
+import {
+  Cylinder,
+  Html,
+  KeyboardControls,
+  OrbitControls,
+} from "@react-three/drei";
+import { animationSet, keyboardMap } from "../../constants/joystick";
 // import { Priest } from '@/models/Priest';
-import { getModelPath } from '@/helpers/path';
+import { getModelPath } from "@/helpers/path";
 import { useEffect, useRef, useState } from "react";
 import { playAudio, stopAudio } from "@/helpers/audio";
 import { Map3 } from "@/models/Map3";
 import { Warrior } from "@/models/Warrior";
 // import { getRandomArbitrary } from "@/helpers/random";
-// import { Trunk } from "@/components/Trunk";
+import { Trunk } from "@/components/Trunk";
 import { Vector3 } from "three";
 import { useCheckpoint } from "@/stores/useCheckpoint";
 import { LoadingScreen } from "@/components/LoadingScreen";
@@ -21,7 +32,7 @@ import { Demon } from "@/models/Demon";
 // import * as THREE from 'three';
 import { CharacterController } from "@/components/CharacterController";
 // import { direction2Points } from "@/helpers/distance";
-// import { Vector } from "@dimforge/rapier3d-compat";
+import { Vector } from "@dimforge/rapier3d-compat";
 import { gameStates, useGame as useLocalGame } from "@/stores/useGame";
 import { GameOver } from "@/components/GameOver";
 import { useCharacter } from "@/stores/useCharacter";
@@ -31,130 +42,156 @@ import { Cross } from "@/models/Cross";
 import { Win } from "@/components/Win";
 import { Caiman } from "@/models/Caiman";
 import { Ocean } from "@/components/Ocean";
+import { CaimanCharacterController } from "@/components/CaimanCharacterController";
+import { CircleOfSpheres } from "@/components/CircleOfSpheres";
 // import { rewards } from "./rewards";
 // import { Reward } from "@/Interfaces/Reward";
 // import { Cross } from "@/models/Cross";
 
 export const Index = () => {
+  const characterURL = getModelPath("warrior");
 
-    const characterURL = getModelPath('warrior');
+  const characterRef = useRef<RapierRigidBody>();
 
-    const characterRef = useRef<RapierRigidBody>();
+  const water1 = useRef<RapierRigidBody>(null);
 
-    const curCheckpoint = useCheckpoint((state) => state.curCheckpoint);
+  const water2 = useRef<RapierRigidBody>(null);
 
-    const setCurCheckpoint = useCheckpoint((state) => state.setCurCheckpoint);
+  const caiman = useRef<RapierRigidBody>(null);
 
-    const setCharacterRef = useCharacter((state) => state.setCharacterRef)
+  const curCheckpoint = useCheckpoint((state) => state.curCheckpoint);
 
-    // const [trunksToShow, setTrunksToShow] = useState<JSX.Element[]>([]);
+  const setCurCheckpoint = useCheckpoint((state) => state.setCurCheckpoint);
 
-    const [velocity, setVelocity] = useState<number>(2.5);
+  const setCharacterRef = useCharacter((state) => state.setCharacterRef);
 
-    const [ecctrlMode, setEcctrlMode] = useState<string | null>(null);
+  // const [trunksToShow, setTrunksToShow] = useState<JSX.Element[]>([]);
 
-    const [loading, setLoading] = useState(false);
+  const [velocity, setVelocity] = useState<number>(2.5);
 
-    const [bullets, setBullets] = useState<TypeBullet[]>([]);
+  const [ecctrlMode, setEcctrlMode] = useState<string | null>(null);
 
-    // @ts-expect-error State types unavailable
-    const curAnimation = useGame((state) => state.curAnimation);
+  const [loading, setLoading] = useState(false);
 
-    // @ts-expect-error State types unavailable
-    const setMoveToPoint = useGame((state) => state.setMoveToPoint);
+  const [bullets, setBullets] = useState<TypeBullet[]>([]);
 
-    const gameState = useLocalGame((state) => state.gameState);
+  const [waterBalls, setWaterBalls] = useState<Vector3[]>([]);
 
-    const setCurLevel = useLocalGame((state) => state.setCurLevel);
+  // @ts-expect-error State types unavailable
+  const curAnimation = useGame((state) => state.curAnimation);
 
-    const [availableRewards, setAvailableRewards] = useState<Reward[]>([]);
+  // @ts-expect-error State types unavailable
+  const setMoveToPoint = useGame((state) => state.setMoveToPoint);
 
-    const storeRewards = useLocalGame((state) => state.rewards);
+  const gameState = useLocalGame((state) => state.gameState);
 
-    const [boosDeath, setBoosDeath] = useState(false);
+  const setCurLevel = useLocalGame((state) => state.setCurLevel);
 
-    // const launchBullet = (position?: Vector) => {
-    //     const demonPosition = position;
-    //     const characterPosition = characterRef.current?.translation()
+  const [availableRewards, setAvailableRewards] = useState<Reward[]>([]);
 
-    //     if (demonPosition && characterPosition) {
-    //         const bulletPosition = demonPosition;
+  const storeRewards = useLocalGame((state) => state.rewards);
 
-    //         const direction = direction2Points(characterPosition, demonPosition)
+  const [boosDeath, setBoosDeath] = useState(false);
 
-    //         const bulletAngle = Math.atan2(direction.x, direction.z);
+  // const launchBullet = (position?: Vector) => {
+  //     const demonPosition = position;
+  //     const characterPosition = characterRef.current?.translation()
 
-    //         const bullet = {
-    //             id: (new Date()).toTimeString(),
-    //             position: vec3(bulletPosition),
-    //             angle: bulletAngle,
-    //         };
+  //     if (demonPosition && characterPosition) {
+  //         const bulletPosition = demonPosition;
 
-    //         setBullets((bullets) => [...bullets, bullet]);
-    //     }
-    // };
+  //         const direction = direction2Points(characterPosition, demonPosition)
 
-    const onHit = (bulletId: string) => {
-        setBullets((bullets) => bullets.filter((bullet) => bullet.id !== bulletId));
-    };
+  //         const bulletAngle = Math.atan2(direction.x, direction.z);
 
-    useEffect(() => {
-        if ([animationSet.walk, animationSet.run].includes(curAnimation) && !loading) {
-            playAudio(curAnimation.toLowerCase());
-        } else {
-            stopAudio();
-        }
-    }, [curAnimation, loading]);
+  //         const bullet = {
+  //             id: (new Date()).toTimeString(),
+  //             position: vec3(bulletPosition),
+  //             angle: bulletAngle,
+  //         };
 
-    useEffect(() => {
-        if (curCheckpoint && curCheckpoint.position) {
-            setLoading(true);
-            setEcctrlMode('PointToMove');
-            setVelocity(14);
-            setMoveToPoint(new Vector3(curCheckpoint.position.x, -0.7, curCheckpoint.position.z));
-        }
-        setCurLevel(3)
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+  //         setBullets((bullets) => [...bullets, bullet]);
+  //     }
+  // };
 
-    useEffect(() => {
-        setCharacterRef(characterRef)
-    }, [characterRef, setCharacterRef])
+  const onHit = (bulletId: string) => {
+    setBullets((bullets) => bullets.filter((bullet) => bullet.id !== bulletId));
+  };
 
-    useEffect(() => {
-        if (storeRewards) {
-            const r = rewards.filter(reward => reward.level === 3)
-            setAvailableRewards(r.filter(reward => !storeRewards.some(stored => stored.id === reward.id)))
-        }
-    }, [storeRewards])
+  const onWaterCollation = (name: string) => {
+    if (name === "w1") {
+      setWaterBalls((balls) => [...balls, vec3(caiman.current?.translation())]);
+    }
+  };
 
-    return (
-        <>
-            {/* <color attach="background" args={["#ececec"]} /> */}
-            <color attach="background" args={['#333333']} />
+  useEffect(() => {
+    if (
+      [animationSet.walk, animationSet.run].includes(curAnimation) &&
+      !loading
+    ) {
+      playAudio(curAnimation.toLowerCase());
+    } else {
+      stopAudio();
+    }
+  }, [curAnimation, loading]);
 
-            {/* LIGHTS */}
-            <ambientLight intensity={0.6} />
-            <directionalLight
-                position={[5, 5, 5]}
-                intensity={0.5}
-                castShadow />
+  useEffect(() => {
+    if (curCheckpoint && curCheckpoint.position) {
+      setLoading(true);
+      setEcctrlMode("PointToMove");
+      setVelocity(14);
+      setMoveToPoint(
+        new Vector3(curCheckpoint.position.x, -0.7, curCheckpoint.position.z)
+      );
+    }
+    setCurLevel(3);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-            <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
-            {/* <KeyboardControls map={keyboardMap}>
-                <Ecctrl ref={characterRef} name={player} mode={ecctrlMode} maxVelLimit={velocity} camInitDis={-6} animated>
-                    <EcctrlAnimation
-                        characterURL={characterURL}
-                        animationSet={animationSet}
-                    >
-                        <Warrior
-                            position-y={-0.9}
-                        />
-                    </EcctrlAnimation>
-                </Ecctrl>
-            </KeyboardControls> */}
+  useEffect(() => {
+    setCharacterRef(characterRef);
+  }, [characterRef, setCharacterRef]);
 
-            {/* {!loading &&
+  useEffect(() => {
+    if (storeRewards) {
+      const r = rewards.filter((reward) => reward.level === 3);
+      setAvailableRewards(
+        r.filter(
+          (reward) => !storeRewards.some((stored) => stored.id === reward.id)
+        )
+      );
+    }
+  }, [storeRewards]);
+
+  return (
+    <>
+      {/* <color attach="background" args={["#ececec"]} /> */}
+      <color attach="background" args={["#333333"]} />
+
+      {/* LIGHTS */}
+      <ambientLight intensity={0.6} />
+      <directionalLight position={[5, 5, 5]} intensity={0.5} castShadow />
+
+      {/* <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} /> */}
+      <KeyboardControls map={keyboardMap}>
+        <Ecctrl
+          ref={characterRef}
+          name={player}
+          mode={ecctrlMode}
+          maxVelLimit={velocity}
+          camInitDis={-6}
+          animated
+        >
+          <EcctrlAnimation
+            characterURL={characterURL}
+            animationSet={animationSet}
+          >
+            <Warrior position-y={-0.9} />
+          </EcctrlAnimation>
+        </Ecctrl>
+      </KeyboardControls>
+
+      {/* {!loading &&
                 <RigidBody colliders={false} type="fixed" position={[-42, -6, 37]}>
                     <CylinderCollider args={[0.5, 1000]} />
                     <Cylinder scale={[1000, 1, 1000]} receiveShadow>
@@ -162,71 +199,96 @@ export const Index = () => {
                 </RigidBody>
             } */}
 
-            {!loading &&
-                <>
-                    <RigidBody colliders={false} type="fixed" position={[-0, -4.5, 187]}>
-                        <Ocean />
-                    </RigidBody>
+      {!loading && (
+        <>
+          <RigidBody type="fixed" colliders={"trimesh"} ccd>
+            <Map3 position={[0, -10, 98]} />
+            <mesh
+              rotation={[-0.5 * Math.PI, 0, 0]}
+              position={[0, 0, 0]}
+              receiveShadow
+            >
+              <planeGeometry args={[0, 0, 1, 1]} />
+              <shadowMaterial transparent opacity={0.2} />
+            </mesh>
+          </RigidBody>
 
-                    <RigidBody colliders={false} type="fixed" position={[-50, -4.5, 187]}>
-                        <Ocean />
-                    </RigidBody>
-                    
-                    <RigidBody type="fixed" colliders={"trimesh"}>
-                        <Map3 position={[0, -10, 98]}/>
-                        <mesh
-                            rotation={[-0.5 * Math.PI, 0, 0]}
-                            position={[0, 0, 0]}
-                            receiveShadow
-                        >
-                            <planeGeometry args={[0, 0, 1, 1]}/>
-                            <shadowMaterial transparent opacity={0.2} />
-                        </mesh>
-                    </RigidBody>
-
-                    {/* <CharacterController position={[0,50,50]} moveSpeed={0.2}>
+          {/* <CharacterController position={[0,50,50]} moveSpeed={0.2}>
                         <Demon />
                     </CharacterController> */}
 
-                    <Caiman scale={60} position={[0, 85, 150]} />
+          <RigidBody
+            ref={water1}
+            name="w1"
+            colliders={"cuboid"}
+            type="fixed"
+            position={[-0, -4.5, 187]}
+          >
+            <Ocean />
+          </RigidBody>
 
-                    {
-                        (bullets).map((bullet: TypeBullet, index: number) => (
-                            <Bullet
-                                key={index}
-                                id={bullet.id}
-                                angle={bullet.angle}
-                                position={bullet.position}
-                                onHit={onHit} />
-                        ))
-                    }
+          <RigidBody
+            ref={water2}
+            name="w2"
+            colliders={"cuboid"}
+            type="fixed"
+            position={[-50, -4.5, 187]}
+          >
+            <Ocean />
+          </RigidBody>
 
-                    {
-                        (availableRewards).map((reward: Reward) => (
-                            <Cross
-                                scale={0.5}
-                                key={reward.id}
-                                reward={reward}
-                                position={reward.position}
-                            />
-                        ))
-                    }
-                </>
-            }
+          <CaimanCharacterController
+            attack={(position) => {
+                console.log('balls', position, caiman.current?.translation());
+                position.y = -4
+                setWaterBalls((balls) => [...balls, vec3(position)]);
+            }}
+            death={() => setBoosDeath(true)}
+            moveSpeed={0.05}
+            ref={caiman}
+            waterCollation={onWaterCollation}
+            position={[0, 100, 170]}
+          >
+            <Caiman scale={60} />
+          </CaimanCharacterController>
 
-            <Html>
-                <LoadingScreen loading={loading} setLoading={setLoading} />
-                {
-                    gameState === gameStates.GAME_OVER &&
-                    <GameOver />
-                }
-                {
-                    boosDeath &&
-                    <Win level="level3" />
-                }
-            </Html>
+          {bullets.map((bullet: TypeBullet, index: number) => (
+            <Bullet
+              key={index}
+              id={bullet.id}
+              angle={bullet.angle}
+              position={bullet.position}
+              onHit={onHit}
+            />
+          ))}
 
-            {/* {trunksToShow} */}
+          {availableRewards.map((reward: Reward) => (
+            <Cross
+              scale={0.5}
+              key={reward.id}
+              reward={reward}
+              position={reward.position}
+            />
+          ))}
+
+          {waterBalls.map((position: Vector3, index: number) => (
+            <CircleOfSpheres
+              key={index}
+              center={position}
+              radius={5}
+              numSpheres={6}
+            />
+          ))}
         </>
-    );
+      )}
+
+      <Html>
+        <LoadingScreen loading={loading} setLoading={setLoading} />
+        {gameState === gameStates.GAME_OVER && <GameOver />}
+        {boosDeath && <Win level="level4" />}
+      </Html>
+
+      {/* {trunksToShow} */}
+    </>
+  );
 };
